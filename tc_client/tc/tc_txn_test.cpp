88691c6aeb756f2797033144f3b3be5522c217fc
Wei Su <suwei@cs.stonybrook.edu>
2019-07-21 15:50:25 -0400

    Declare variables only when needed

--- tc_client/tc/tc_txn_test.cpp
+++ tc_client/tc/tc_txn_test.cpp
@@ -74,30 +74,24 @@ static bool posix_integrity(const std::string base, const std::string path,
   full_path /= path;
 
   FILE *fp = fopen(full_path.c_str(), "rb");
-  char *buffer = nullptr;
-  size_t fsize = 0;
-  bool result;
   if (fp == nullptr) {
     return false;
   }
 
   /* check size first */
   fseek(fp, 0, SEEK_END);
-  fsize = ftell(fp);
-  if (fsize != len) {
-    result = false;
-    goto end;
+  if (ftell(fp) != len) {
+    fclose(fp);
+    return false;
   }
   rewind(fp);
 
   /* then check content */
-  buffer = (char *)malloc(len);
+  char *buffer = (char *)malloc(len);
   assert(buffer);
 
   fread(buffer, len, 1, fp);
-  result = (memcmp(buffer, data, len) == 0);
-
-end:
+  bool result = (memcmp(buffer, data, len) == 0);
   fclose(fp);
   return result;
 }
@@ -155,14 +149,13 @@ TYPED_TEST_P(TcTxnTest, BadFileCreation) {
   const std::vector<std::string> paths{
       "bad-creation/good/a", "bad-creation/good/b", "bad-creation/good/c",
       "bad-creation/good/d", "bad-creation/bad/",   "bad-creation/good/f"};
-  vfile *files;
 
   /* create dirs */
   ASSERT_TRUE(tc::sca_mkdir(basedir, 0777));
   ASSERT_TRUE(tc::sca_mkdir(dir1, 0777));
   ASSERT_TRUE(tc::sca_mkdir(dir2, 0000));
 
-  files = tc::vec_open_simple(paths, O_CREAT, 0666);
+  vfile *files = tc::vec_open_simple(paths, O_CREAT, 0666);
   EXPECT_EQ(files, nullptr);
 
   /* expect the files NOT to exist */
@@ -185,11 +178,10 @@ TYPED_TEST_P(TcTxnTest, BadFileCreation2) {
       "bad-creation2/1",       "bad-creation2/a/2",
       "bad-creation2/a/b/3",   "bad-creation2/a/b/c/4",
       "bad-creation2/a/b/c/d", "bad-creation2/a/b/c/d/e/5"};
-  vfile *files;
 
   ASSERT_TRUE(tc::vec_mkdir_simple(dirs, 0777));
 
-  files = tc::vec_open_simple(paths, O_CREAT, 0666);
+  vfile *files = tc::vec_open_simple(paths, O_CREAT, 0666);
   EXPECT_EQ(files, nullptr);
 
   for (int i = 0; i < 4; ++i) {
@@ -207,17 +199,16 @@ TYPED_TEST_P(TcTxnTest, BadOpenWithTrunc) {
   std::vector<std::string> paths{"bad-opentrunc/a", "bad-opentrunc/b",
                                  "bad-opentrunc/c", "bad-opentrunc/d",
                                  "bad-opentrunc/e", "bad-opentrunc/f"};
-  const int n = paths.size();
-  vfile *files;
-  struct viovec *v1;
 
   /* create dir */
   ASSERT_TRUE(tc::sca_mkdir(dir, 0777));
   /* create files and write some data */
-  files = tc::vec_open_simple(paths, O_CREAT | O_WRONLY, 0666);
+  vfile *files = tc::vec_open_simple(paths, O_CREAT | O_WRONLY, 0666);
   ASSERT_NE(files, nullptr);
-  v1 = (struct viovec *)calloc(n, sizeof(*v1));
+  const int n = paths.size();
+  struct viovec *v1 = (struct viovec *)calloc(n, sizeof(*v1));
   ASSERT_NE(v1, nullptr);
+
   for (int i = 0; i < n; ++i) {
     viov2file(&v1[i], &files[i], 0, datasize, getRandomBytes(datasize));
   }
@@ -254,19 +245,18 @@ TYPED_TEST_P(TcTxnTest, BadCreationWithExisting) {
   const std::vector<std::string> paths2{"bad-creation3/a",  "bad-creation3/b",
                                         "bad-creation3/c",  "bad-creation3/d",
                                         "bad-creation3/no", "bad-creation3/f"};
-  vfile *files1, *files2;
 
   ASSERT_TRUE(tc::sca_mkdir(dir1, 0777));
   ASSERT_TRUE(tc::sca_mkdir(dir2, 0777));
 
   /* create files in paths1[] */
-  files1 = tc::vec_open_simple(paths1, O_CREAT, 0666);
+  vfile *files1 = tc::vec_open_simple(paths1, O_CREAT, 0666);
   ASSERT_NE(files1, nullptr);
   vec_close(files1, paths1.size());
 
   /* create files in paths2[] - this will fail and rollback, but we expect that
    * files in paths1 won't be removed */
-  files2 = tc::vec_open_simple(paths2, O_CREAT, 0666);
+  vfile *files2 = tc::vec_open_simple(paths2, O_CREAT, 0666);
   EXPECT_EQ(files2, nullptr);
 
   /* check that files in paths1[] are not removed during rollback */
@@ -292,12 +282,11 @@ TYPED_TEST_P(TcTxnTest, BadRemove) {
   const std::vector<std::string> paths{"bad-remove/a", "bad-remove/b",
                                        "bad-remove/c", "bad-remove/no",
                                        "bad-remove/d"};
-  vfile *files;
 
   ASSERT_TRUE(tc::sca_mkdir(dir, 0777));
 
   /* create files in paths[] */
-  files = tc::vec_open_simple(paths, O_CREAT, 0666);
+  vfile *files = tc::vec_open_simple(paths, O_CREAT, 0666);
   ASSERT_NE(files, nullptr);
   vec_close(files, paths.size());
 
@@ -327,19 +316,17 @@ TYPED_TEST_P(TcTxnTest, BadRemoveCheckContent) {
   const std::vector<std::string> paths{"bad-remove2/a", "bad-remove2/b",
                                        "bad-remove2/c", "bad-remove2/no",
                                        "bad-remove2/e"};
-  const int n = paths.size();
-  struct viovec *iov = nullptr;
-  vfile *files;
 
   /* create base dir */
   ASSERT_TRUE(tc::sca_mkdir(dir, 0777));
 
   /* create files in paths[] */
-  files = tc::vec_open_simple(paths, O_CREAT | O_WRONLY, 0666);
+  vfile *files = tc::vec_open_simple(paths, O_CREAT | O_WRONLY, 0666);
   ASSERT_NE(files, nullptr);
 
   /* write to these files */
-  iov = (struct viovec *)calloc(n, sizeof(*iov));
+  const int n = paths.size();
+  struct viovec *iov = (struct viovec *)calloc(n, sizeof(*iov));
   ASSERT_NE(iov, nullptr);
   for (int i = 0; i < n; ++i) {
     viov2file(&iov[i], &files[i], 0, datasize, getRandomBytes(datasize));
@@ -380,12 +367,11 @@ TYPED_TEST_P(TcTxnTest, BadLink) {
                                      "bad-link/d", "bad-link/e", "bad-link/f"};
   const std::vector<std::string> dest{"bad-link/1", "bad-link/2", "bad-link/3",
                                       "bad-link/d", "bad-link/5", "bad-link/6"};
-  vfile *files;
 
   /* create base dir */
   ASSERT_TRUE(tc::sca_mkdir(dir, 0777));
   /* create files in src[] */
-  files = tc::vec_open_simple(src, O_CREAT, 0666);
+  vfile *files = tc::vec_open_simple(src, O_CREAT, 0666);
   ASSERT_NE(files, nullptr);
   vec_close(files, src.size());
 
@@ -438,20 +424,15 @@ TYPED_TEST_P(TcTxnTest, BadWrite) {
   const std::vector<std::string> paths{"bad-write/a", "bad-write/b",
                                        "bad-write/c", "bad-write/d",
                                        "bad-write/e", "bad-write/f"};
-  int n = paths.size();
-  std::vector<int> flags(n);
-  std::vector<mode_t> modes(n);
-  vfile *files;
-  struct viovec *v1, *v2;
 
   /* create base dir */
   ASSERT_TRUE(tc::sca_mkdir(dir, 0777));
   /* create files in paths[] and write some data */
-  files = tc::vec_open_simple(paths, O_CREAT | O_WRONLY, 0666);
+  vfile *files = tc::vec_open_simple(paths, O_CREAT | O_WRONLY, 0666);
   ASSERT_NE(files, nullptr);
-  v1 = (struct viovec *)calloc(n, sizeof(*v1));
-  v2 = (struct viovec *)calloc(n, sizeof(*v2));
-  ASSERT_TRUE(v1 && v2);
+  const int n = paths.size();
+  struct viovec *v1 = (struct viovec *)calloc(n, sizeof(*v1));
+  ASSERT_TRUE(v1);
   for (int i = 0; i < n; ++i) {
     viov2file(&v1[i], &files[i], 0, datasize, getRandomBytes(datasize));
   }
@@ -461,11 +442,15 @@ TYPED_TEST_P(TcTxnTest, BadWrite) {
    * One of the file is opened in RDONLY mode, so the whole compound
    * will fail and rollback. Thus the content of other files should
    * remain that of v1[i].data */
+  std::vector<int> flags(n);
+  std::vector<mode_t> modes(n);
   std::fill_n(flags.begin(), n, O_RDWR);
   std::fill_n(modes.begin(), n, 0666);
   flags[4] = O_RDONLY;
   files = tc::vec_open(paths, flags, modes);
   ASSERT_NE(files, nullptr);
+  struct viovec *v2 = (struct viovec *)calloc(n, sizeof(*v2));
+  ASSERT_TRUE(v2);
   for (int i = 0; i < n; ++i) {
     viov2file(&v2[i], &files[i], 0, datasize, getRandomBytes(datasize));
   }
@@ -494,32 +479,31 @@ TYPED_TEST_P(TcTxnTest, BadWriteMiddle) {
   const std::vector<std::string> paths{"bad-write2/a", "bad-write2/b",
                                        "bad-write2/c", "bad-write2/d",
                                        "bad-write2/e", "bad-write2/f"};
-  int n = paths.size();
-  std::vector<int> flags(n);
-  std::vector<mode_t> modes(n);
-  vfile *files;
-  struct viovec *v1, *v2;
 
   /* create base dir */
   ASSERT_TRUE(tc::sca_mkdir(dir, 0777));
   /* create files in paths[] and write random data */
-  files = tc::vec_open_simple(paths, O_CREAT | O_WRONLY, 0666);
+  vfile *files = tc::vec_open_simple(paths, O_CREAT | O_WRONLY, 0666);
   ASSERT_NE(files, nullptr);
-  v1 = (struct viovec *)calloc(n, sizeof(*v1));
-  v2 = (struct viovec *)calloc(n, sizeof(*v2));
-  ASSERT_TRUE(v1 && v2);
+  int n = paths.size();
+  struct viovec *v1 = (struct viovec *)calloc(n, sizeof(*v1));
+  ASSERT_TRUE(v1);
   for (int i = 0; i < n; ++i) {
     viov2file(&v1[i], &files[i], 0, datasize, getRandomBytes(datasize));
   }
   EXPECT_OK(vec_write(v1, n, true));
   EXPECT_OK(vec_close(files, n));
   /* Issue a WRITE compound cmd that has a invalid req in the middle */
+  std::vector<int> flags(n);
+  std::vector<mode_t> modes(n);
   std::fill_n(flags.begin(), n, O_RDWR);
   std::fill_n(modes.begin(), n, 0666);
   flags[4] = O_RDONLY;
   files = tc::vec_open(paths, flags, modes);
   ASSERT_NE(files, nullptr);
   /* this time let's write at random offset and random size */
+  struct viovec *v2 = (struct viovec *)calloc(n, sizeof(*v2));
+  ASSERT_TRUE(v2);
   for (int i = 0; i < n; ++i) {
     size_t offset = stdexp::randint((size_t)1024, datasize);
     size_t len = stdexp::randint((size_t)1, datasize - offset);
@@ -551,26 +535,22 @@ TYPED_TEST_P(TcTxnTest, BadWriteExpanding) {
   const std::vector<std::string> paths{"bad-write3/a", "bad-write3/b",
                                        "bad-write3/c", "bad-write3/d",
                                        "bad-write3/e", "bad-write3/f"};
-  int n = paths.size();
-  std::vector<int> flags(n);
-  std::vector<mode_t> modes(n);
-  vfile *files;
-  struct viovec *v1, *v2;
-
   /* create base dir */
   ASSERT_TRUE(tc::sca_mkdir(dir, 0777));
   /* create files in paths[] and write random data */
-  files = tc::vec_open_simple(paths, O_CREAT | O_WRONLY, 0666);
+  vfile *files = tc::vec_open_simple(paths, O_CREAT | O_WRONLY, 0666);
   ASSERT_NE(files, nullptr);
-  v1 = (struct viovec *)calloc(n, sizeof(*v1));
-  v2 = (struct viovec *)calloc(n, sizeof(*v2));
-  ASSERT_TRUE(v1 && v2);
+  const int n = paths.size();
+  struct viovec *v1 = (struct viovec *)calloc(n, sizeof(*v1));
+  ASSERT_TRUE(v1);
   for (int i = 0; i < n; ++i) {
     viov2file(&v1[i], &files[i], 0, datasize, getRandomBytes(datasize));
   }
   EXPECT_OK(vec_write(v1, n, true));
   EXPECT_OK(vec_close(files, n));
   /* Issue a WRITE compound cmd that has a invalid req in the middle */
+  std::vector<int> flags(n);
+  std::vector<mode_t> modes(n);
   std::fill_n(flags.begin(), n, O_RDWR);
   std::fill_n(modes.begin(), n, 0666);
   flags[4] = O_RDONLY;
@@ -578,6 +558,8 @@ TYPED_TEST_P(TcTxnTest, BadWriteExpanding) {
   ASSERT_NE(files, nullptr);
   /* this time let's write at random offset and random size. However
    * these requests will expand the original file */
+  struct viovec *v2 = (struct viovec *)calloc(n, sizeof(*v2));
+  ASSERT_TRUE(v2);
   for (int i = 0; i < n; ++i) {
     size_t offset = stdexp::randint(datasize / 2, datasize);
     size_t len = stdexp::randint(datasize - offset, maxsize);
